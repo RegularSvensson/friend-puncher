eb64ade58576ee39de5899c7dedd7bf377da159c
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-08 16:05:38 +0200

    Add spaghetti code to hide/show images by clicking buttons

--- js/app.js
+++ js/app.js
@@ -1,2 +1,30 @@
 var friends = $('.friend');
 var button = $('button');
+friends.hide();
+$('#friend1').show();
+
+$( "#button1" ).click(function() {
+	friends.hide();
+	$('#friend1').show();
+});
+
+$( "#button2" ).click(function() {
+	friends.hide();
+	$('#friend2').show();
+});
+
+$( "#button3" ).click(function() {
+	friends.hide();
+	$('#friend3').show();
+});
+
+$( "#button4" ).click(function() {
+	friends.hide();
+	$('#friend4').show();
+});
+
+$( "#button5" ).click(function() {
+	friends.hide();
+	$('#friend5').show();
+});
+
