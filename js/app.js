8e2f5c4be0e31a22a1bc9aaf4608eac122152add
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-08 16:09:58 +0200

    Refactor code to hide/show images on button clicks

--- js/app.js
+++ js/app.js
@@ -1,31 +1,17 @@
 var friends = $('.friend');
-var button = $('button');
 var buttons = $('button');
 
 friends.hide();
 $('#friend1').show();
 
-$( "#button1" ).click(function() {
-	friends.hide();
-	$('#friend1').show();
-});
 
-$( "#button2" ).click(function() {
-	friends.hide();
-	$('#friend2').show();
-});
+function bindButtonToFriend(idNumber) {
+	$( "#button" + idNumber ).click(function() {
+		friends.hide();
+		$('#friend' + idNumber).show();
+	})
+}
 
-$( "#button3" ).click(function() {
-	friends.hide();
-	$('#friend3').show();
-});
-
-$( "#button4" ).click(function() {
-	friends.hide();
-	$('#friend4').show();
-});
-
-$( "#button5" ).click(function() {
-	friends.hide();
-	$('#friend5').show();
-});
+for (var i=1; i <= buttons.length; i++) {
+	bindButtonToFriend(i);
+}
