5bdbd1a48aba5dc2edb3b64187b100c368f34f59
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-08 16:14:54 +0200

    Add increment counter when images clicked spaghetti

--- js/app.js
+++ js/app.js
@@ -15,3 +15,33 @@ function bindButtonToFriend(idNumber) {
 for (var i=1; i <= buttons.length; i++) {
 	bindButtonToFriend(i);
 }
+
+$( "#friend1" ).click(function() {
+	var count = $('#counter1').text();
+	count = parseInt(count) + 1;
+	$('#counter1').text(count);
+});
+
+$( "#friend2" ).click(function() {
+	var count = $('#counter2').text();
+	count = parseInt(count) + 1;
+	$('#counter2').text(count);
+});
+
+$( "#friend3" ).click(function() {
+	var count = $('#counter3').text();
+	count = parseInt(count) + 1;
+	$('#counter3').text(count);
+});
+
+$( "#friend4" ).click(function() {
+	var count = $('#counter4').text();
+	count = parseInt(count) + 1;
+	$('#counter4').text(count);
+});
+
+$( "#friend5" ).click(function() {
+	var count = $('#counter5').text();
+	count = parseInt(count) + 1;
+	$('#counter5').text(count);
+});
